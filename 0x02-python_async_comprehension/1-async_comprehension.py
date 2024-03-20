29991dd0e8f7e8897c19dedd0d1aa443d7f303b3
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-03-20 16:15:47 +0100

    ok

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Module to demonstrate Async Comprehension"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ Coroutine to generate random float numbers """
+    return [x async for x in async_generator()]
+
+
+if __name__ == "__main__":
+    async def main():
+        print(await async_comprehension())
+
+    asyncio.run(main())
